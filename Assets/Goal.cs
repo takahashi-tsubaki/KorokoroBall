8462619f47342bff772d7f3205b97b88b3dcaefc
LE2B_13_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-05-17 16:02:54 +0900

    ゴール後に次シーンへ行くボタンの表示　まで完成

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    public GameObject ClearText;
+    public GameObject NextBotton;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("GOAL!!");
+        ClearText.SetActive(true);
+        NextBotton.SetActive(true);
     }
     // Start is called before the first frame update
     void Start()
