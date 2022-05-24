3b4a5a2aa8f7d8234f76df611d19df9c67273857
LE2B_13_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-05-24 15:54:21 +0900

    解像度の固定まで完成

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-
+    public GameManager gameManager;
+    public void Start()
+    {
+        GameObject managerObject = GameObject.Find("GameManager");
+        gameManager = managerObject.GetComponent<GameManager>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            gameManager.addCoinCount();
             Destroy(gameObject);
         }
     }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
-  
+
     public void SceneReset()
     {
         string acticeSceneName = SceneManager.GetActiveScene().name;
@@ -14,8 +15,22 @@ public class GameManager : MonoBehaviour
 
     public void ChangeScene(string NextScene)
     {
-        SceneManager.LoadScene(NextScene);  
+        SceneManager.LoadScene(NextScene);
     }
 
+    private int coinCount;
+    private void Start()
+    {
+        Screen.SetResolution(1920,1080,false);
+        Application.targetFrameRate = 60;
+        coinCount = 0;
+    }
+    public Text textComponent;
+    public void addCoinCount()
+    {
+        coinCount = coinCount + 1;
+        Debug.Log("coinCount:" + coinCount);
+        textComponent.text = "coinCount : " + coinCount;
+    }
 
 }
